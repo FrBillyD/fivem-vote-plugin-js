7690cdf88317bec1749aec044f4507bb13eb7cf0
Billy D <contact@billyd.fr>
2020-12-13 14:08:05 +0100

    Sources

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'BillyD'
+description 'Plugin de vote pour les serveurs Grand Theft Auto V sous FiveM'
+version '1.0.0'
+
+server_script 'server.js'
+
+server_only 'yes'
